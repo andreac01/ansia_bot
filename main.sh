4cde81b669bab916fa0e3407e2633303cf616091
andreac01 <andrea01.cerutti@gmail.com>
2024-10-22 00:17:34 +0200

    typo

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the directory containing main.py
-cd ~/ansia_bot
+cd <base_directory>/ansia_bot
 # Activate the virtual environment
 source ~/venvs/telegram_venv/bin/activate
 # Run the Python script
