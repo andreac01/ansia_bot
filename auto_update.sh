252c3001e8b97199d58b57688d76713346641aa7
andreac01 <andrea01.cerutti@gmail.com>
2024-10-28 19:20:11 +0100

    add auto update

---
+++ auto_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to the directory containing main.py
+cd <base_directory>/ansia_bot
+# Pull the latest changes from the remote repository
+git pull origin master
+# Activate the virtual environment
+source ~/venvs/telegram_venv/bin/activate
+# Install any new dependencies
+pip install -r requirements.txt
+# Kill the existing Python process
+pkill -f request_handler.py
+# Run the Python script
+python3 request_handler.py
